8c27b72800637416ae24b2f893227278c1e36ed0
Paweł Pisarek <pisarekp73@gmail.com>
2014-12-24 22:02:58 +0100

    po przeniesienu z działajacym  menu

--- src/Obrazki/wbazieBundle/Controller/ObrazkiwKategoriiController.php
+++ src/Obrazki/wbazieBundle/Controller/ObrazkiwKategoriiController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ObrazkiwKategoriiController extends Controller
 {
@@ -25,6 +26,59 @@ class ObrazkiwKategoriiController extends Controller
             'entities' => $entities
         );
     }
+
+    /**
+     * @Route("/index/{grupa}",name="menuk")
+     */
+    public function szukajAction($grupa)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+
+        $entities = $em->getRepository('ObrazkiwbazieBundle:Przed');
+
+
+
+        $query = $entities->createQueryBuilder('p')
+            ->select('p')
+            ->leftJoin('ObrazkijakoProduktyBundle:Grupa', 'g', 'WITH', 'p.grupa = g.id')
+            ->where('g.nazwa=:grupa')
+            ->setParameter('grupa',$grupa)
+            ->getQuery();
+
+
+
+
+
+        $result = $query->getResult();
+        if(sizeof($result) == 0){
+
+            $result = $em->getRepository('ObrazkiwbazieBundle:Przed')->findAll();
+        }
+
+//
+//        exit(\Doctrine\Common\Util\Debug::dump($result));
+
+//        $entities = $em->getRepository('ObrazkiwbazieBundle:Przed')->findBy(array('grupa_id' => $grupa));
+//         var_dump($em->getRepository('ObrazkiwbazieBundle:Przed')->innerJoin('grupa_id')->where('.id = :user_id'));
+////        $qb->join('u.Group', 'g', 'WITH', 'u.status = ?1')
+////             $qb->join('Przed.grupa_id', 'g', 'WITH', 'grupa.id = ?1')
+//
+//        exit(\Doctrine\Common\Util\Debug::dump($entities));
+//
+
+        return $this->render(
+            '@Obrazkiwbazie/ObrazkiwKategorii/index.html.twig',
+            array(
+                'entities' => $result,
+
+            )
+        );
+
+
+
+    }
+
 
     /**
      * @Route("/{id}.html", name="show_produkt")
@@ -68,41 +122,29 @@ class ObrazkiwKategoriiController extends Controller
 //jutro pomyśle
 
 
-    
-
     /**
      * @Route("plotno/new", name="plotno_new")
      * @Template()
      */
-    public function plotnoAction( Request $request)
+    public function plotnoAction(Request $request)
     {
         $atrybuty = new atrybuty();
         $entity = new typy();
         $entity->setNazwa('płótno');
-
         $form = $this->createForm(new atrybutyType(), $atrybuty);
-
-
         $form->add('submit', 'submit', array('label' => 'Create'));
 //        var_dump($form);
 //        exit();
-
         $form->handleRequest($request);
-
-
         if ($form->isValid()) {
-
-
-//
             $entity->setAtrybut($atrybuty);
-
             $em = $this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
 
 //            exit(\Doctrine\Common\Util\Debug::dump($form->getData()));
 
-           return $this->render(
+            return $this->render(
                 '@ObrazkijakoProdukty/typy/show.html.twig',
                 array(
                     'entity' => $entity,
@@ -111,9 +153,7 @@ class ObrazkiwKategoriiController extends Controller
                 )
             );
 
-
         }
-
 
         return array(
             'entity' => $entity,
