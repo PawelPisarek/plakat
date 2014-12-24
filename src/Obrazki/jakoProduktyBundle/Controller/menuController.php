a696521c74269c1ad9a90c5bb4249f23cffb5fb3
Paweł Pisarek <pisarekp73@gmail.com>
2014-12-24 22:27:07 +0100

    poprawa menu

--- src/Obrazki/jakoProduktyBundle/Controller/menuController.php
+++ src/Obrazki/jakoProduktyBundle/Controller/menuController.php
@@ -17,4 +17,32 @@ class menuController extends Controller
         $entities=$em->getRepository('ObrazkijakoProduktyBundle:Grupa')->findAll();
         return array('entities' => $entities);
     }
+
+
+    /**
+     * @Route("/index/{grupa}",name="menuk")
+     */
+    public function menuAction($grupa)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $entities = $em->getRepository('ObrazkiwbazieBundle:Przed');
+        $query = $entities->createQueryBuilder('p')
+            ->select('p')
+            ->leftJoin('ObrazkijakoProduktyBundle:Grupa', 'g', 'WITH', 'p.grupa = g.id')
+            ->where('g.nazwa=:grupa')
+            ->setParameter('grupa',$grupa)
+            ->getQuery();
+        $result = $query->getResult();
+        if(sizeof($result) == 0){
+
+            $result = $em->getRepository('ObrazkiwbazieBundle:Przed')->findAll();
+        }
+        return $this->render(
+            '@Obrazkiwbazie/ObrazkiwKategorii/index.html.twig',
+            array(
+                'entities' => $result,
+
+            )
+        );
+    }
 }
--- src/Obrazki/wbazieBundle/Controller/ObrazkiwKategoriiController.php
+++ src/Obrazki/wbazieBundle/Controller/ObrazkiwKategoriiController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 class ObrazkiwKategoriiController extends Controller
 {
     /**
-     * @Route("/index", name="kategoirie" )
+     * @Route("/index/", name="kategoirie" )
      * @Template()
      */
     public function indexAction()
@@ -26,59 +26,6 @@ class ObrazkiwKategoriiController extends Controller
             'entities' => $entities
         );
     }
-
-    /**
-     * @Route("/index/{grupa}",name="menuk")
-     */
-    public function szukajAction($grupa)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-
-        $entities = $em->getRepository('ObrazkiwbazieBundle:Przed');
-
-
-
-        $query = $entities->createQueryBuilder('p')
-            ->select('p')
-            ->leftJoin('ObrazkijakoProduktyBundle:Grupa', 'g', 'WITH', 'p.grupa = g.id')
-            ->where('g.nazwa=:grupa')
-            ->setParameter('grupa',$grupa)
-            ->getQuery();
-
-
-
-
-
-        $result = $query->getResult();
-        if(sizeof($result) == 0){
-
-            $result = $em->getRepository('ObrazkiwbazieBundle:Przed')->findAll();
-        }
-
-//
-//        exit(\Doctrine\Common\Util\Debug::dump($result));
-
-//        $entities = $em->getRepository('ObrazkiwbazieBundle:Przed')->findBy(array('grupa_id' => $grupa));
-//         var_dump($em->getRepository('ObrazkiwbazieBundle:Przed')->innerJoin('grupa_id')->where('.id = :user_id'));
-////        $qb->join('u.Group', 'g', 'WITH', 'u.status = ?1')
-////             $qb->join('Przed.grupa_id', 'g', 'WITH', 'grupa.id = ?1')
-//
-//        exit(\Doctrine\Common\Util\Debug::dump($entities));
-//
-
-        return $this->render(
-            '@Obrazkiwbazie/ObrazkiwKategorii/index.html.twig',
-            array(
-                'entities' => $result,
-
-            )
-        );
-
-
-
-    }
-
 
     /**
      * @Route("/{id}.html", name="show_produkt")
@@ -94,33 +41,9 @@ class ObrazkiwKategoriiController extends Controller
         }
 
         return array(
-            'entity' => $entity        // ...
+           'entity' => $entity        // ...
         );
     }
-
-//
-//    /**
-//     * Creates a form to create a typy entity.
-//     *
-//     * @param typy $entity The entity
-//     *
-//     * @return \Symfony\Component\Form\Form The form
-//     */
-//    private function createCreateForm(typy $entity)
-//    {
-//        $form = $this->createForm(new typyType(), $entity, array(
-//                'action' => $this->generateUrl('typy_create'),
-//                'method' => 'POST',
-//            ));
-//
-//        $form->add('submit', 'submit', array('label' => 'Create'));
-//
-//        return $form;
-//    }
-
-
-//jutro pomyśle
-
 
     /**
      * @Route("plotno/new", name="plotno_new")
@@ -133,17 +56,12 @@ class ObrazkiwKategoriiController extends Controller
         $entity->setNazwa('płótno');
         $form = $this->createForm(new atrybutyType(), $atrybuty);
         $form->add('submit', 'submit', array('label' => 'Create'));
-//        var_dump($form);
-//        exit();
         $form->handleRequest($request);
         if ($form->isValid()) {
             $entity->setAtrybut($atrybuty);
             $em = $this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
-
-//            exit(\Doctrine\Common\Util\Debug::dump($form->getData()));
-
             return $this->render(
                 '@ObrazkijakoProdukty/typy/show.html.twig',
                 array(
@@ -152,15 +70,11 @@ class ObrazkiwKategoriiController extends Controller
                     'typy_delete' => $entity->getId(),
                 )
             );
-
         }
-
         return array(
             'entity' => $entity,
             'form' => $form->createView(),
         );
-
-
     }
 
 
